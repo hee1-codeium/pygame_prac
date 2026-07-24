bbb250abc344f13c9c1896894185cbe699f42261
임희원 <heeewon0324@gmail.com>
2026-07-24 17:46:06 +0900

    infinte_run_3.py 이미지 파일 경로 수정
    선생님이 작업.
    이미지 파일 경로를 수정하여
    이미지가 적용될 수 있도록 함.

--- infinte_run_3.py
+++ infinte_run_3.py
@@ -9,9 +9,9 @@ clock=py.time.Clock()
 dino_image = None
 cactus_image = None
 try:
-    dino_image = py.image.load(".\img\dinosaur.png")
+    dino_image = py.image.load(".\dinosaur.png")
     dino_image = py.transform.scale(dino_image, (100,90))
-    cactus_image = py.image.load(".\img\cactus.png")
+    cactus_image = py.image.load(".\cactus.png")
     cactus_image = py.transform.scale(cactus_image, (105,110))
 except:
     print("이미지 파일을 찾을 수 없습니다! 기본 사각형으로 대체하여 게임을 시작합니다.")
